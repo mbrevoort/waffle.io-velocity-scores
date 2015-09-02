22232abf189c71652cddc6bdec83ef19bf8c15e3
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2015-09-01 20:19:16 -0600

    Tweaked styles, removed icon, disable waffle scores

--- js/content.js
+++ js/content.js
@@ -1,47 +1,56 @@
-function sumScores(scores) {
-  var total = 0;
-  $.each(scores, function () {
-    var v = $(this).text();
-    if (v != '?') {
-      total += parseInt(v, 10);
+(function() {
+
+    function sumScores(scores) {
+        var total = 0;
+        $.each(scores, function() {
+            var v = $(this).text();
+            if (v != '?') {
+                total += parseInt(v, 10);
+            }
+        });
+        return total;
     }
-  });
-  return total;
-}
-function extractScore(t) {
-  var score, matches = t.match(/(?:^|\s)\(([\d]+)\)(?:$|\s)/);
-  return matches ? matches[1] : '?';
-}
-function updateCardScore(card, score) {
-  var footer = card.find('.footer');
- 
-  if (footer.find('.score').length) {
-    footer.find('.score strong').text(score);
-  } else {
-    footer.append('<div class="score"><strong>' + score + '</strong> <i class="fa fa-lightbulb-o"></i></div>');
-  }
-}
-function updateColumnTotal(column, total) {
-  var header = column.find('.column-header');
- 
-  if (header.find('.total-score').length) {
-    header.find('.total-score strong').text(total);
-  } else {
-    header.append('<span class="total-score"><strong>' + total + '</strong> <i class="fa fa-lightbulb-o"></i></span>');
-  }
-}
-function processCardScore() {
-  var t = $(this).find('.title').val();
-  updateCardScore($(this), extractScore(t));
-}
-function tallyColumn() {
-  updateColumnTotal($(this), sumScores($(this).find('.footer .score strong')));
-}
 
-function fetchScores() {
-  $('.card-body').each(processCardScore);null
-  $('.column-ct').each(tallyColumn);null
-  setTimeout(fetchScores, 2000)
-}
+    function extractScore(t) {
+        var score, matches = t.match(/(?:^|\s)\(([\d]+)\)(?:$|\s)/);
+        return matches ? matches[1] : '?';
+    }
 
-fetchScores()
+    function updateCardScore(card, score) {
+        var footer = card.find('.footer');
+
+        if (footer.find('.score').length) {
+            footer.find('.score strong').text(score);
+        } else if (score != '?') {
+            footer.append('<div class="score"><strong>' + score + '</strong></div>');
+        }
+    }
+
+    function updateColumnTotal(column, total) {
+        var header = column.find('.column-header');
+
+        if (header.find('.total-score').length) {
+            header.find('.total-score strong').text(total);
+        } else if (total != '?') {
+            header.append('<span class="total-score"><strong>' + total + '</strong>');
+        }
+    }
+
+    function processCardScore() {
+        var t = $(this).find('.title').val();
+        updateCardScore($(this), extractScore(t));
+    }
+
+    function tallyColumn() {
+        updateColumnTotal($(this), sumScores($(this).find('.footer .score strong')));
+    }
+
+    function fetchScores() {
+        $('.card-body').each(processCardScore);
+        $('.column-ct').each(tallyColumn);
+        setTimeout(fetchScores, 2000)
+    }
+
+    fetchScores()
+
+})()
